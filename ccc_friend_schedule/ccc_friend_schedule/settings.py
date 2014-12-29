421fd74740107c87fd966b07abc591177e648abe
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-12-29 02:04:33 +0100

    Adding USE_X_FORWARDED_HOST

--- ccc_friend_schedule/ccc_friend_schedule/settings.py
+++ ccc_friend_schedule/ccc_friend_schedule/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ccc_friend_schedule'
+    'ccc_friend_schedule',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
